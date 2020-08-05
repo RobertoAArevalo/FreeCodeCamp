4a2b70a558b2a16a133bb7af398f069d31c13fc9
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-05 08:10:14 -0700

    afa

---
+++ exercise1.1-1.9.py
@@ -0,0 +1,40 @@
+#Exercise 1.1
+# the function of a secondary memory in a computer is to store programs and data. It retains information even if the
+#power is off. It is typically slower than main memory.
+
+#Exercise 1.2
+# A program is "a set of instructions that specifies a computation."
+
+#Exercise 1.3
+# An interpreter is used to read the source code of a program that is written by the programmer.
+# It parses the source code, and interprets instructions quickly.
+#A compiler needs the entire program in a file. The compiler can then run a process to translate the high level source
+# code into machine language. The compiler can then put the resulting machine language into a file to be executed at a
+# later time.
+
+#Exercise 1.4
+#The Python interpreter has machine code inside of it.
+
+#Exercise 1.5
+# The grammer in the code needs to checked due to the SyntaxError. Print should have been written instead of primt.
+
+#Exercise 1.6
+# Avariable such as "x" is stored after a Python line finishes in the Main Memory which stores data.
+
+#Exercise 1.7
+x = 43
+x = x + 1
+print (x)
+
+#Results = 44
+
+#Exercise 1.8
+# The Central Processing unit would be like the human brain. The Main Memory would the part of the brain that stores
+#memory. The Secondary Memory would be like github in that memory out lives the brain. The input device obtains data
+#from the outside world, so this would be comparible to the eyes obtaining information. The output device is used to '
+# is useful for displaying results of a program or storing them in a file which can be compared to fat storing energy.
+
+#Exercise 1.9
+# A syntax error is when the grammar rules of Python have been violated. If the grammar is corrected, the syntax error
+#may be fixed.
+
