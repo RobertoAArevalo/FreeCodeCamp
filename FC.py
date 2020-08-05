e07047fc9b7dbbccb42ea3f09ffb5b8d1f54b802
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-05 01:09:35 -0700

    afa

--- FC.py
+++ FC.py
@@ -1,2 +1,16 @@
-#this a comment on python
-a = b = c = 1
+#this a PFE - Variables,
+# Expressions, and Statements
+
+inp = input('Europe floor?')
+usf = int(inp) + 1
+print('US floor', usf)
+
+inp = 4
+print('US floor', usf)
+
+# Exercise 2
+
+
+
+#This is PFE - Int. Expressions
+
